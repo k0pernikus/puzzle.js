ed8f01ffb78eccdb503b5deec28e9b59ddc55c0c
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2012-12-11 17:44:29 +0100

    created playfield

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function puzzle($) {
-    var tileAbstract = {
+    var tileProperty = {
         position: {
             x: null,
             y: null
@@ -16,23 +16,29 @@
         isNeighbour: function(tile) {
             return ($.inArray(tile, this.neighbors));
         },
-        fillImage: function(image, x, y){
+        fillImage: function(image, x, y, tileSize) {
             var ctx = this.canvas.getContext('2d');
             ctx.drawImage(image, x * this.size.width, y * this.size.height, this.size.width, this.size.height, 0, 0, this.canvas.width, this.canvas.height);
         },
-        init: function(x, y, tileSize) {
+        bind: function() {
+            var that = this;
+            $(this.canvas).bind('click', function() {
+                console.log(that.position, that.x, that.y);
+            });
+        },
+        init: function(x, y, size) {
+            this.position = Object.create(this.position);
             this.position.x = x;
             this.position.y = y;
-            this.size = tileSize;
+            this.size = size;
+
             this.canvas = document.createElement('canvas');
             this.canvas.width = this.size.width;
             this.canvas.height = this.size.height;
-            var that = this;
-            $(this.canvas).bind('click', function(){
-               console.log(that.position);
-            });
 
             document.body.appendChild(this.canvas);
+
+            this.bind();
         }
     }
 
@@ -40,45 +46,33 @@
         playgroundCtx: null,
         baseImage: null,
         size: {
-            columns: 2,
-            rows: 2
+            columns: 5,
+            rows: 5
         },
         tiles: [],
         $viewport: null,
         image: new Image(),
         shuffle: function() {
-            /* make a mess */
+            /* make a mess :D */
         },
-        splitImageToTiles: function(image, x, y) {
-            calcluateTileSize();
-        },
-        calculateTileSize: function() {
-        },
-
         cutImageUp: function(numColsToCut, numRowsToCut, image) {
-            var tileSize = {
-                width: Math.ceil(image.width / numColsToCut),
-                height: Math.ceil(image.height / numRowsToCut)
-            };
-
-            debugger;
-            for(var x = 0; x < numColsToCut; ++x) {
-                for(var y = 0; y < numRowsToCut; ++y) {
-                    var tile = Object.create(tileAbstract);
+            var tileSize = this.calculateTileSize(image, numColsToCut, numRowsToCut);
+            for (var y = 0; y < numRowsToCut; ++y) {
+                for (var x = 0; x < numColsToCut; ++x) {
+                    var tile = Object.create(tileProperty);
+                    tile.position = Object.create(tileProperty.position);
                     tile.init(x, y, tileSize);
-                    tile.fillImage(image, x, y);
-                    $(tile.canvas).addClass(x.toString()).addClass(y.toString());
-                    tile.position.x = x;
-                    tile.position.y = y;
-
+                    tile.fillImage(image, x, y, tileSize);
                     this.tiles.push(tile);
                     tile = null;
                 }
             }
-
-
-            console.log(this.tiles);
-
+        },
+        calculateTileSize: function(image, colums, rows) {
+            return {
+                width: Math.ceil(image.width / colums),
+                height: Math.ceil(image.height / rows)
+            };
         },
         init: function(viewport) {
             this.$viewport = $(viewport);
@@ -96,5 +90,4 @@
             p.init(this);
         });
     });
-
 }(jQuery));
