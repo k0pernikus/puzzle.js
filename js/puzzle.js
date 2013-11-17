46a03186ba597eeeca2818e5e62538e930745a29
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-11-17 03:43:50 +0100

    refactoring

--- js/puzzle.js
+++ js/puzzle.js
@@ -25,16 +25,16 @@
         canvas: null,
         $canvas: null,
         traverseAllConnectedTiles: {},
-        addNeighbor: function(tile) {
+        addNeighbor: function (tile) {
             this.neighbors.push(tile);
         },
-        registerNeighbor: function(tile) {
+        registerNeighbor: function (tile) {
             this.connectedNeighbors.push(tile);
             tile.connectedNeighbors.push(this);
         },
-        handlePotentialNeighbour: function(canvas, targetTile) {
+        handlePotentialNeighbour: function (canvas, targetTile) {
             var that = this;
-            this.neighbors.forEach(function(tile) {
+            this.neighbors.forEach(function (tile) {
                 if (canvas.isEqualNode(tile.canvas)) {
                     var droppedTile = tile;
 
@@ -44,38 +44,55 @@
                     droppedTile.animateToPosition(targetTile.coordinatesInPixel.left + xDiff * that.size.width, targetTile.coordinatesInPixel.top + yDiff * that.size.height);
                     droppedTile.registerNeighbor(targetTile);
 
+
+
+                    var connectedTiles = [];
+                    function traverse(o) {
+                        if (o.hasOwnProperty("connectedNeighbors")) {
+                            o["connectedNeighbors"].forEach(function (tile) {
+                                if ($.inArray(tile, connectedTiles) == -1) {
+                                    connectedTiles.push(tile);
+                                    traverse(tile);
+                                }
+                            });
+                        }
+                    }
+                    traverse(droppedTile);
+                    connectedTiles.forEach(function (tile) {
+                        tile.moveToCorrectPositionRelativeTo(droppedTile);
+                    });
                 }
             });
         },
-        fillImage: function(image, x, y, tileSize) {
+        fillImage: function (image, x, y, tileSize) {
             var ctx = this.canvas.getContext('2d');
             ctx.drawImage(image, x * tileSize.width, y * tileSize.height, tileSize.width, tileSize.height, 0, 0, tileSize.width, tileSize.height);
         },
-        getRandomNumberInRange: function(LowerRange, UpperRange) {
+        getRandomNumberInRange: function (LowerRange, UpperRange) {
             return Math.floor(Math.random() * (UpperRange - LowerRange + 1)) + LowerRange;
         },
-        preRandomize: function() {
+        preRandomize: function () {
             var x = this.positionWithinGrid.x * this.size.width;
             var y = this.positionWithinGrid.y * this.size.height;
 
             this.correctCoordinatesInPixel = {x: x, y: y};
         },
-        randomize: function() {
+        randomize: function () {
             var x = this.getRandomNumberInRange(this.size.width, 900) - this.size.width;
             var y = this.getRandomNumberInRange(this.size.height, 500) - this.size.height;
 
             this.animateToPosition(x, y);
         },
-        animateToPosition: function(left, top) {
+        animateToPosition: function (left, top) {
             this.$canvas.animate({
                 top: top,
                 left: left
             });
         },
-        moveToCorrectPosition: function() {
+        moveToCorrectPosition: function () {
             this.animateToPosition(this.correctCoordinatesInPixel.x, this.correctCoordinatesInPixel.y);
         },
-        moveToCorrectPositionRelativeTo: function(tile) {
+        moveToCorrectPositionRelativeTo: function (tile) {
             var position = tile.$canvas.position();
             var dX = (this.positionWithinGrid.x - tile.positionWithinGrid.x)
             var dY = (this.positionWithinGrid.y - tile.positionWithinGrid.y);
@@ -84,36 +101,33 @@
 
             this.animateToPosition(left, top);
         },
-        bind: function() {
+        bind: function () {
             var that = this;
 
-            this.$canvas.on('updateClasses', function(event, id) {
-            });
-
-            this.$canvas.on('click', function() {
-                $(that.neighbors).each(function() {
+            this.$canvas.on('click', function () {
+                $(that.neighbors).each(function () {
                     this.$canvas.toggleClass('markNeighbors');
 
                 });
 
             });
 
-            $document.bind('randomize', function() {
+            $document.bind('randomize', function () {
                 that.preRandomize();
                 that.moveToCorrectPosition();
                 that.randomize();
             });
 
-            $document.bind('solve', function() {
+            $document.bind('solve', function () {
                 that.moveToCorrectPosition();
             });
 
-            $document.bind('getByPosition', function(event, x, y) {
+            $document.bind('getByPosition', function (event, x, y) {
                 if (that.positionWithinGrid.x == x && that.positionWithinGrid.y == y) {
                 }
             });
 
-            $document.bind('addNeighbor', function(event, possibleNeighbor) {
+            $document.bind('addNeighbor', function (event, possibleNeighbor) {
                 if (that === possibleNeighbor) {
                     return;
                 }
@@ -131,23 +145,23 @@
                 }
             });
         },
-        init: function(x, y, tileSizeInPixels, $viewport) {
+        init: function (x, y, tileSizeInPixels, $viewport) {
             var Template = {
                 x: null,
                 y: null
             }
 
 
-
             this.positionWithinGrid = Object.create(Template);
             this.coordinatesInPixel = Object.create(Template);
             this.correctCoordinatesInPixel = Object.create(Template);
 
-            this.neighbors = new Array();
-            this.connectedNeighbors = new Array();
+            this.neighbors = [];
+            this.connectedNeighbors = [];
 
             this.positionWithinGrid.x = x;
             this.positionWithinGrid.y = y;
+
             this.size = tileSizeInPixels;
 
             this.canvas = document.createElement('canvas');
@@ -159,9 +173,9 @@
             var that = this;
             this.$canvas.draggable({
                 revert: false,
-                start: function(e, ui) {
+                start: function (e, ui) {
                 },
-                drag: function(e, ui) {
+                drag: function (e, ui) {
                     var previousPosition = $(this).data('previousLocation');
 
                     if (!previousPosition) {
@@ -176,32 +190,33 @@
                     that.allConnectedTiles = [];
                     function traverse(o) {
                         if (o.hasOwnProperty("connectedNeighbors")) {
-                            o["connectedNeighbors"].forEach(function(tile){
-                                if ($.inArray(tile, that.allConnectedTiles ) == -1) {
-                                    that.allConnectedTiles .push(tile);
+                            o["connectedNeighbors"].forEach(function (tile) {
+                                if ($.inArray(tile, that.allConnectedTiles) == -1) {
+                                    that.allConnectedTiles.push(tile);
                                     traverse(tile);
                                 }
                             });
                         }
                     }
+
                     traverse(that);
 
 
-                    that.allConnectedTiles.forEach(function(tile){
+                    that.allConnectedTiles.forEach(function (tile) {
                         var $canvas = tile.$canvas;
                         var position = $canvas.position();
                         var left = position.left;
                         var top = position.top;
                         $canvas.css('left', left + leftOffset);
-                        $canvas.css('top', top+ topOffset);
+                        $canvas.css('top', top + topOffset);
                     });
 
                     $(this).data('previousLocation', currentPosition);
                 },
-                stop: function(e, ui) {
+                stop: function (e, ui) {
                     $(this).data('previousLocation', null);
                     that.coordinatesInPixel = null;
-                    that.allConnectedTiles.forEach(function(tile){
+                    that.allConnectedTiles.forEach(function (tile) {
                         if (that !== tile) {
                             tile.moveToCorrectPositionRelativeTo(that);
                         }
@@ -214,7 +229,7 @@
             var that = this;
             this.$canvas.droppable({
                 tolerance: "pointer",
-                drop: function(event, ui) {
+                drop: function (event, ui) {
                     that.coordinatesInPixel = that.$canvas.position();
                     var targetTile = that;
                     /**
@@ -242,7 +257,7 @@
         tiles: [],
         $viewport: null,
         image: new Image(),
-        cutImageIntoTiles: function(numColsToCut, numRowsToCut, image) {
+        cutImageIntoTiles: function (numColsToCut, numRowsToCut, image) {
             var tileSize = this.calculateTileSize(image, numColsToCut, numRowsToCut);
             for (var y = 0; y < numRowsToCut; ++y) {
                 for (var x = 0; x < numColsToCut; ++x) {
@@ -255,26 +270,22 @@
                 }
             }
 
-            this.tiles.forEach(function(tile) {
+            this.tiles.forEach(function (tile) {
                 $document.trigger('addNeighbor', tile);
             });
-
-//            this.tiles.forEach(function(tile) {
-//                tile.$canvas.trigger('updateClasses', {id: 1});
-//            });
         },
-        calculateTileSize: function(image, columns, rows) {
+        calculateTileSize: function (image, columns, rows) {
             return {
                 width: Math.ceil(image.width / columns),
                 height: Math.ceil(image.height / rows)
             };
         },
-        bind: function() {
-            $document.on('notifyConnectedTilesAmount', function(event, data) {
+        bind: function () {
+            $document.on('notifyConnectedTilesAmount', function (event, data) {
                 console.log(data);
             });
         },
-        init: function(viewport) {
+        init: function (viewport) {
             this.$viewport = $(viewport);
             this.baseImage = this.$viewport.find('img')[0];
             this.cutImageIntoTiles(this.size.columns, this.size.rows, this.baseImage);
@@ -283,13 +294,13 @@
         }
     }
 
-    $window.load(function() {
+    $window.load(function () {
         /**
          * NOTE: window.load ensures that image is fully loaded
          */
 
         var $puzzles = $(".puzzlejs_viewport");
-        $puzzles.each(function() {
+        $puzzles.each(function () {
             Object.create(PuzzleProperty).init(this);
         });
 
